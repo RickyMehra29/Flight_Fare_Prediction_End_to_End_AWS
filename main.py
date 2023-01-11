26d027b03cbde66b638b473ce1645dba7aa6db85
Ricky Mehra <Ricky.mehra299@gmail.com>
2023-01-11 21:19:37 +0530

    updated

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ file_path="/config/workspace/input_files/Test_dataset.xlsx"
 
 if __name__=="__main__":
     try:
-        start_point = start_training_pipeline()
-        print(start_point)
+        #start_point = start_training_pipeline()
+        #print(start_point)
 
-        #output_file = start_batch_prediction(input_file_path=file_path)
-        #print(output_file)
+        output_file = start_batch_prediction(input_file_path=file_path)
+        print(output_file)
 
     except Exception as e:
        raise FlightFareException(e, sys)
